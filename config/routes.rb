ddc273212efdb383002fa722b644bf65b7108697
ashtonbcheung <70730294+ashtonbcheung@users.noreply.github.com>
2024-12-11 21:04:02 +0000

    Revert "generated files"
    This reverts commit 7ad137c4327009e6ff32003dda5f1c057839c594.

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,4 @@
 Rails.application.routes.draw do
-  get 'follow_requests/create'
-  get 'follow_requests/update'
-  get 'follow_requests/destroy'
-  get 'likes/create'
-  get 'likes/destroy'
-  get 'comments/create'
-  get 'comments/destroy'
-  get 'photos/index'
-  get 'photos/show'
-  get 'photos/new'
-  get 'photos/create'
-  get 'photos/edit'
-  get 'photos/update'
-  get 'photos/destroy'
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
